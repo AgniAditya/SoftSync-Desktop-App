6974478518295c135443d9ffbcf6e004d2f48f1a
AgniAditya <agniaditya05@gmail.com>
2025-09-24 17:36:05 +0530

    removing isDev function to fix the path

--- src/process/pathResolver.ts
+++ src/process/pathResolver.ts
@@ -1,10 +1,6 @@
 import path from 'path'
-import { isDev } from './utils.js'
 import { app } from 'electron'
 
 export function getPreloadPath(){
-    return path.join(
-        app.getAppPath(), isDev() ? '.' : '..',
-        '/dist-electron/preload.cjs'
-    )
+    return path.join(app.getAppPath(),'/dist-electron/preload.cjs')
 }
