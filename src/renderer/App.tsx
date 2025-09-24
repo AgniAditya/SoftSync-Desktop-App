a0b33127281456ebaf6408febb7e0a9c08385baa
AgniAditya <agniaditya05@gmail.com>
2025-09-24 17:39:12 +0530

    update the UI

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,29 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import softsyncLogo from './assets/logo.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={softsyncLogo} className="logo softsync" alt="SoftSync logo" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>SoftSync - Automate your work</h1>
     </>
   )
 }
