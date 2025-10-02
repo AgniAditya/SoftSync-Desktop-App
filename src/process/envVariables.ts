f103a456eeb98babe4e2dac83c4973a4d99dc799
AgniAditya <agniaditya05@gmail.com>
2025-10-02 19:54:16 +0530

    fix the env varibale for production ready

--- src/process/envVariables.ts
+++ src/process/envVariables.ts
@@ -1,8 +1,15 @@
 import dotenv from 'dotenv'
+import path from "path";
+import { isDev } from './utils.js';
+import { app } from 'electron';
 
-dotenv.config()
+// Decide path depending on environment
+const envPath = isDev()
+  ? path.join(app.getAppPath(), ".env")
+  : path.join(process.resourcesPath, ".env");
+
+dotenv.config({ path: envPath })
 
 export const variables = {
     openRouterApiKey: process.env.OPENROUTER_API_KEY,
-    rendererPort: process.env.RENDERER_PORT,
 }
