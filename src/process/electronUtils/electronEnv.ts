27d6c85436d23d3d3c042ed3dd89ae043b70ed14
AgniAditya <agniaditya05@gmail.com>
2025-10-09 19:01:09 +0530

    refactor: streamline environment variable path resolution in envVariables

--- src/process/electronUtils/electronEnv.ts
+++ src/process/electronUtils/electronEnv.ts
@@ -5,6 +5,11 @@ export function getPreloadPath(){
     return path.join(app.getAppPath(),'/dist-electron/preload.cjs')
 }
 
+export function envPath(){
+    return isDev() ? path.join(app.getAppPath(), ".env")
+      : path.join(process.resourcesPath, ".env");
+}
+
 export function isDev() : boolean {
     return process.env.NODE_ENV === 'development'
 }
--- src/process/envVariables.ts
+++ src/process/envVariables.ts
@@ -1,14 +1,7 @@
 import dotenv from 'dotenv'
-import path from "path";
-import { isDev } from './electronUtils/electronEnv.js';
-import { app } from 'electron';
+import { envPath } from './electronUtils/electronEnv.js';
 
-// Decide path depending on environment
-const envPath = isDev()
-  ? path.join(app.getAppPath(), ".env")
-  : path.join(process.resourcesPath, ".env");
-
-dotenv.config({ path: envPath })
+dotenv.config({ path: envPath() })
 
 export const variables = {
     openRouterApiKey: process.env.OPENROUTER_API_KEY,
